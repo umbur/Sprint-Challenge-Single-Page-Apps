e9474cee11ceefc1a74b537d9dbd3ca1c91edf3f
umbur <you@example.com>
2019-07-26 10:29:27 -0600

    Part 1 Completed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+const CharacterDiv = styled.div`
+`;
+
+export default function CharacterCard (props) {
+  let character = props.character;
+  return (
+      <CharacterDiv>
+        <h3>{character.name}</h3>
+        <div><img src={character.image}  alt={character.name}/></div>
+        <hr />
+        <ul>
+          <li>Species: {character.species}</li>
+          <li>Gender: {character.gender}</li>
+          <li>Origin: {character.origin.name}</li>
+          <li>Status: {character.status}</li>
+        </ul>
+      </CharacterDiv>
+  );
 }
